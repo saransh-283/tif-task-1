436bb14b6e8438ca761cefb9e94d3086a65a38b5
saransh-283 <saranshmm@gmail.com>
2023-02-10 23:33:02 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <Fragment></Fragment>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "brand-orange": "#E23744",
+      "brand-blue": "#0E2368",
+      "brand-lightgray": "#444957",
+      "brand-gray": "#424961",
+    },
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
